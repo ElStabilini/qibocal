fc5b07cfc7ce22e4a8e71b05a5d1cc928f61dd97
ElStabilini <el.stabilini@gmail.com>
2026-09-09 14:05:25 +0200

    test time of flight update

--- src/qibocal/protocols/signal_experiments/time_of_flight_readout.py
+++ src/qibocal/protocols/signal_experiments/time_of_flight_readout.py
@@ -99,7 +99,7 @@ def _acquisition(
         acq_handle = list(sequence.channel(platform.qubits[qubit].acquisition))[-1].id
         data.data[qubit] = results[acq_handle]
 
-        print({f"configs.{ro_channel}.delay": results.time_of_flights[qubit]})
+        print({f"configs.{ro_channel}.delay : to be computed"})
     return data
 
 
